40c6fe1aa44ba837d79313a7811a89ded3b78beb
riyaz <riyazahamedquadri@gmail.com>
2022-07-24 21:21:08 +0530

    first commit

---
+++ src/main/java/com/example/demo/TestunitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestunitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestunitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/test/UnitTest.java
@@ -0,0 +1,17 @@
+package com.example.demo.test;
+
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+	
+
+	@Test
+	public void save() {
+		System.out.println("Savein spring() method executed");
+	}
+	
+	@Test
+	public void update() {
+		System.out.println("update() method executed");
+	}
+}
